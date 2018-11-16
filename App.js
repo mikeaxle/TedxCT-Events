cc4d385c7a91b006d454b3b26566ab18a233aa98
mikeaxle <mikeaxle2006@gmail.com>
2018-11-16 21:56:53 +0200

    codepush added

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Tedx Cape Town Event Programme Mobile Appt
+ * http://tedxcapetown.org
  *
  * @format
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import RootStack from './src/utils/RootStack'
-
+import codePush from "react-native-code-push";
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-        <RootStack />
 
-    );
-  }
+class App extends Component<Props> {
+    render() {
+        return (
+            <RootStack/>
+
+        );
+    }
 }
+
+// codepush options
+let codePushOptions = {
+    checkFrequency: codePush.CheckFrequency.ON_APP_START,
+    installMode: codePush.InstallMode.IMMEDIATE,
+    updateDialog: true,
+}
+
+
+// wrap app in codepush
+export default App = codePush(codePushOptions)(App)
--- android/app/src/main/java/com/tedxct/MainApplication.java
+++ android/app/src/main/java/com/tedxct/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,35 +18,42 @@ import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
+
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNFirebasePackage(),
+                    new RNFirebaseFirestorePackage(),
+                    new CodePush("LgWeBe1qX4TAE7p8kZEgu29bNhd3HJ1rv526X", MainApplication.this, BuildConfig.DEBUG)
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-              new MainReactPackage(),
-              new RNFirebasePackage(),
-              new RNFirebaseFirestorePackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
